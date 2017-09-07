83c2330507d4d8dd624f1f3c57fbbeb54f9cf67b
artycake <arrtycake.work@gmail.com>
2017-09-07 09:57:45 +0300

    Initial Commit

---
+++ Facility Test Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facility Test Application
+//
+//  Created by Arty Cake on 9/7/17.
+//  Copyright © 2017 Arty Cake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
